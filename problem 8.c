a2adf651d77dbb25a5a8d192c44b8a8792adbae7
Md.Rakibul Hasan <32368310+Rakibul66@users.noreply.github.com>
2018-05-22 22:00:52 +0600

    Add files via upload

---
+++ problem 8.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    float x;
+    a=250;
+    b=85;
+    c=25;
+    x=a/(b-c);
+    printf("the value is:x=%f\n",x);
+    a=300;
+    b=70;
+    c=a/(b-c);
+    printf("x=%f\n");
+
+}
