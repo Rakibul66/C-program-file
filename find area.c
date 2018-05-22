7ff3815189615379f8bb15210062478f4ed266eb
Md.Rakibul Hasan <32368310+Rakibul66@users.noreply.github.com>
2018-05-22 21:57:53 +0600

    Create find area.c

---
+++ find area.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+#define PI 3.1416
+int main()
+{  float r,area;
+ printf("Enter the radius of circle in cm:\n");
+ scanf("%f",&r);
+ area=r*r*PI;
+ printf("\nthe area of circle is:%f squarcm",area);
+ return 0;
+
+
+
+}
