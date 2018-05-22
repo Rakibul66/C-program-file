f89db488ed643f0b84f4172a77d90d2e3055a625
Md.Rakibul Hasan <32368310+Rakibul66@users.noreply.github.com>
2018-05-22 22:00:11 +0600

    Add files via upload

---
+++ temperature centigrade.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main()
+{
+    float c,f;
+    printf("Enter temp in farenheight:");
+    scanf("%f",&f);
+    c=(f-32)/1.8;
+    printf("tempareture in a celcsius:%f",c);
+    return 0;
+}
+
